ccb01d2dc3f4b589fa0ffd31f87775417bad091e
realvesd <43302742+realvesd@users.noreply.github.com>
2021-06-15 15:25:54 -0300

    Add files via upload

---
+++ src/main/java/br/com/blog/model/Postagem.java
@@ -0,0 +1,72 @@
+package br.com.blog.model;
+
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "postagens")
+public class Postagem implements Serializable{
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, lenght = 200)
+	private String titulo;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Postagem other = (Postagem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Column(nullable = false, lenght = 200)
+	private String post;
+	
+	
+	
+	public Postagem() {
+		super();
+	}
+	public Postagem(Long id, String titulo, String post) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.post = post;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getPost() {
+		return post;
+	}
+	public void setTexto(String post) {
+		this.post = post;
+	}
+
+}
